c484d519e3f99a68f09899161a252066b4784d83
Guilherme Bigois <guilhermebigois@outlook.com>
2023-05-31 00:21:12 -0300

    Created Spring profile settings

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- Database seeding for USERS
+insert into users (cpf, email, name, birth_date, gender) values ('68515553872', 'marcela-rezende92@carolpessoa.com.br', 'Marcela Liz Vera Rezende', '1943-02-14', 'F');
+insert into users (cpf, email, name, birth_date, gender) values ('29081928619', 'renataluziaporto@asconinternet.com.br', 'Renata Luzia Francisca Porto', '1957-04-08', 'F');
+insert into users (cpf, email, name, birth_date, gender) values ('25276887560', 'caroline_larissa_assuncao@infonet.com.br', 'Caroline Larissa Assunção', '1983-02-09', 'F');
+insert into users (cpf, email, name, birth_date, gender) values ('76290569236', 'elza_daconceicao@flir.com.br', 'Elza Flávia da Conceição', '2003-04-19', 'F');
+insert into users (cpf, email, name, birth_date, gender) values ('29436331002', 'diego.ruan.viana@bhcervejas.com.br', 'Diego Ruan Viana', '1983-04-09', 'M');
