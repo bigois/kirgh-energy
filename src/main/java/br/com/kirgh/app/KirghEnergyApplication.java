3eb7b7a61bf7ef00bc2538a85d246544bee859ff
Guilherme Bigois <guilhermebigois@outlook.com>
2023-05-30 21:44:31 -0300

    Initial commit

---
+++ src/main/java/br/com/kirgh/app/KirghEnergyApplication.java
@@ -0,0 +1,13 @@
+package br.com.kirgh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirghEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirghEnergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kirgh/app/KirghEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kirgh.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirghEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
