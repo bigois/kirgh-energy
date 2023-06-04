38bfd4817298657833bf11571a7f0330ca76801c
Guilherme Bigois <guilhermebigois@outlook.com>
2023-06-04 16:41:10 -0300

    Changed @NotEmpty to @NotBlank due String use

--- src/main/java/br/com/kirgh/app/dtos/UserDTO.java
+++ src/main/java/br/com/kirgh/app/dtos/UserDTO.java
@@ -7,7 +7,7 @@ import org.hibernate.validator.constraints.br.CPF;
 import java.util.Date;
 
 public record UserDTO(
-        @NotEmpty(message = "cannot be null or empty")
+        @NotBlank(message = "cannot be null or empty")
         @Size(min = 2, max = 150, message = "size must be between {min} and {max}")
         @Pattern(regexp = "[A-zÀ-ú\s]+", message = "must contain only letters")
         String name,
@@ -16,20 +16,16 @@ public record UserDTO(
         @Past(message = "must be before the current date")
         Date birthDate,
 
-        @NotNull(message = "cannot be null or empty")
+        @NotBlank(message = "cannot be null or empty")
         UserGender gender,
 
-        @NotEmpty(message = "cannot be null or empty")
+        @NotBlank(message = "cannot be null or empty")
         @Size(min = 11, max = 11, message = "must have {min} characters")
         @CPF
         String cpf,
 
-        @NotEmpty(message = "cannot be null or empty")
+        @NotBlank(message = "cannot be null or empty")
         @Email(message = "is invalid")
         String email
-
-        // @NotEmpty(message = "cannot be empty")
-        // UserRelation relation
 ) {
-
 }
