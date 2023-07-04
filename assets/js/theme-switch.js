1ad98f348b9adbc7faa8002826c717b41377beef
Guilherme Bigois <guilhermebigois@outlook.com>
2023-07-04 00:58:27 -0300

    Wiki completed
    Co-authored-by: Kelvin Natal <KelvinNatal@users.noreply.github.com>
    Co-authored-by: Ronilsonb2 <Ronilsonb2@users.noreply.github.com>
    Co-authored-by: oFrancoGui <oFrancoGui@users.noreply.github.com>

---
+++ assets/js/theme-switch.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", function() {
+  const toggleDarkMode = document.getElementById("theme-toggle");
+
+  if (localStorage.getItem('theme') === 'light') {
+    setTheme('light');
+  } else {
+    setTheme('dark');
+  }
+
+  jtd.addEvent(toggleDarkMode, 'click', function(){
+    const currentTheme = getTheme();
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    localStorage.setItem('theme', newTheme);
+    setTheme(newTheme);
+  });
+
+  function getTheme() {
+    return document.documentElement.classList.contains('dark-mode') ? 'dark' : 'light';
+  }
+
+  function setTheme(theme) {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+      document.documentElement.classList.remove("transition");
+    }, 500)
+
+    if (theme === 'dark') {
+      toggleDarkMode.innerHTML = `<svg width='18px' height='18px'><use href="#svg-moon"></use></svg>`;
+      document.documentElement.classList.add('dark-mode');
+      document.documentElement.classList.remove('light-mode');
+    } else {
+      toggleDarkMode.innerHTML = `<svg width='18px' height='18px'><use href="#svg-sun"></use></svg>`;
+      document.documentElement.classList.add('light-mode');
+      document.documentElement.classList.remove('dark-mode');
+    }
+  }
+});
--- docs/technologies/technologies.md
+++ docs/technologies/technologies.md
@@ -2,7 +2,6 @@
 layout: default
 title: Technologies
 nav_order: 3
-has_children: true
 permalink: /docs/technologies
 ---
 
@@ -10,3 +9,51 @@ permalink: /docs/technologies
 
 This section describes the technologies directly focused on developing this application (e.g., frameworks, dependencies, IDEs, etc.), as well as technologies that assist in creating code documentation, code versioning, project documentation, team meeting minutes, and so on, not directly related to code development.
 {: .fs-6 .fw-300 }
+
+### IDEs
+
+- IntelliJ IDEA
+- IntelliJ IDEA Community
+- Visual Studio Code
+
+### Dependencies
+
+- Bean Validation
+- Swagger Open API
+- Lombok
+- H2 Database
+- Spring Data JPA
+- Spring Web
+- JSON
+- Spring Boot
+
+### Plataforms
+
+- Railway
+- Postman
+- GitHub
+- AWS
+- Google Docs
+- Stack Overflow
+
+### Plugins
+
+- Gradle Javadoc
+- Mintlify IA Doc Writer
+
+### Templates
+
+- Just the Docs
+- al-folio
+- mmCEsim
+- Spring Initializr
+
+### Communication
+
+- Discord
+- WhatsApp
+
+### Tools
+
+- Ditto
+- ChatGPT
