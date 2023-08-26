96df8e9e9ecb4fb253d113111c4446775c2c6628
matheus-diefenthaeler <matheus.dieff@gmail.com>
2023-08-25 23:39:34 -0300

    feat: add Swagger for address controller endpoints

--- src/main/java/br/com/kirgh/app/controllers/AddressController.java
+++ src/main/java/br/com/kirgh/app/controllers/AddressController.java
@@ -64,7 +64,7 @@ public class AddressController {
             }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
             @ApiResponse(responseCode = "404", description = "NOT FOUND - Parent id not found", content = @Content(examples = {
                     @ExampleObject(summary = "Invalid Parent Id",
-                            value = "{\"street\": \"Rua Jose\", \"zipCode\":\"08421520\", \"number\": 185, \"city\":\"São Paulo\", \"state\":\"SP\", \"parentId\":\"b1f6147e-4c1f-4362-930e-a77a7ee707fd\"}")
+                            value = "{\"model\": \"cannot be null or empty\",\"timestamp\": \"2023-08-26T01:51:40.016469Z\"}")
             }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
             @ApiResponse(responseCode = "409", description = "CONFLICT - address already exists to user", content = @Content(examples = {
                     @ExampleObject(summary = "Conflict parent id",
@@ -85,6 +85,20 @@ public class AddressController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response.toString());
     }
 
+    @Operation(
+            summary = "Get addresses paginated ",
+            description = "Method to get a list of addresses paginated with JSON response with the address info"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(examples = {
+                    @ExampleObject(summary = "Get an list of paginated Addresses.",
+                            value = "{\"content\": [{\"id\": \"26ead1cd-c0d6-47bd-bb79-f0aeb4b897bb\",\"zipCode\": \"03701010\",\"street\": \"Avenida Gabriela Mistral\",\"number\": \"670\",\"city\": \"São Paulo\",\"state\": \"SP\"}],\"pageable\": {\"sort\": {\"empty\": false,\"sorted\": true,\"unsorted\": false},\"offset\": 0,\"pageNumber\": 0,\"pageSize\": 1,\"paged\": true,\"unpaged\": false},\"last\": false,\"totalPages\": 3,\"totalElements\": 3,\"size\": 1,\"number\": 0,\"sort\": {\"empty\": false,\"sorted\": true,\"unsorted\": false},\"first\": true,\"numberOfElements\": 1,\"empty\": false}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR - Something goes wrong", content = @Content(examples = {
+                    @ExampleObject(summary = "Internal Server Error",
+                            value = "{\"message\": \"something goes wrong\", \"timestamp\": \"2023-08-26T00:21:30.426833300Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     @GetMapping()
     public ResponseEntity<Page<Address>> getFilteredAddresses(Pageable pageable, @RequestParam Map<String, String> filters) {
         Utils.removePageableKeysFromFilter(filters);
@@ -93,24 +107,110 @@ public class AddressController {
         return ResponseEntity.status(HttpStatus.OK).body(addresses);
     }
 
+    @Operation(
+            summary = "Get address information by id ",
+            description = "Method to get a address by id returning a JSON response with the address info"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(examples = {
+                    @ExampleObject(summary = "Get an Address.",
+                            value = "{\"id\": \"26ead1cd-c0d6-47bd-bb79-f0aeb4b897bb\",\"zipCode\": \"03701010\",\"street\": \"Avenida Gabriela Mistral\",\"number\": \"670\",\"city\": \"São Paulo\",\"state\": \"SP\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND - address id not found", content = @Content(examples = {
+                    @ExampleObject(summary = "Invalid Address Id",
+                            value = "{\"message\": \"address not found\", \"timestamp\": \"2023-08-26T00:23:12.454577100Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR - Something goes wrong", content = @Content(examples = {
+                    @ExampleObject(summary = "Internal Server Error",
+                            value = "{\"message\": \"something goes wrong\", \"timestamp\": \"2023-08-26T00:21:30.426833300Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Address> getAllAddressInfoById(@PathVariable UUID id) {
         Address address = addressService.getAllAddressInfoById(id);
         return ResponseEntity.status(HttpStatus.OK).body(address);
     }
 
+    @Operation(
+            summary = "Get address information by id (with or without appliances) ",
+            description = "Method to get a address by id returning a JSON response with the address info"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(examples = {
+                    @ExampleObject(summary = "Get an Address.",
+                            value = "{\"addressData\": {\"id\": \"26ead1cd-c0d6-47bd-bb79-f0aeb4b897bb\",\"zipCode\": \"03701010\",\"street\": \"Avenida Gabriela Mistral\",\"number\": \"670\",\"city\": \"São Paulo\",\"state\": \"SP\"},\"appliances\": [{\"id\": \"0415be05-b5e9-49b1-a51b-60ac820fb2e6\",\"name\": \"Fogão\",\"brand\": \"Atlas\",\"model\": \"Mônaco\",\"power\": \"V110\"}]}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND - address id not found", content = @Content(examples = {
+                    @ExampleObject(summary = "Invalid Address Id",
+                            value = "{\"message\": \"address not found\", \"timestamp\": \"2023-08-26T00:23:12.454577100Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR - Something goes wrong", content = @Content(examples = {
+                    @ExampleObject(summary = "Internal Server Error",
+                            value = "{\"message\": \"something goes wrong\", \"timestamp\": \"2023-08-26T00:21:30.426833300Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     @GetMapping("/{id}/appliances")
     public ResponseEntity<AddressCompleteInfoDTO> getAllAppliancesBoundAddress(@PathVariable UUID id) {
         AddressCompleteInfoDTO addressCompleteInfoDTO = addressService.getAllAppliancesBoundAddress(id);
         return ResponseEntity.status(HttpStatus.OK).body(addressCompleteInfoDTO);
     }
 
+    @Operation(
+            summary = "Update address (with or without parent relation)",
+            description = "Method for updating a address and returning a JSON response with the new user's Address info"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(examples = {
+                    @ExampleObject(summary = "Update an Address.",
+                            value = "{\"id\": \"26ead1cd-c0d6-47bd-bb79-f0aeb4b897bb\",\"zipCode\": \"12332112\",\"street\": \"Rual Abril\",\"number\": \"70\",\"city\": \"São Paulo\",\"state\": \"SP\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST - Invalid body content", content = @Content(examples = {
+                    @ExampleObject(summary = "Invalid content",
+                            value = "{\"name\": \"must contain only letters\", \"timestamp\": \"2023-08-26T01:00:05.809350200Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND - Owner id not found", content = @Content(examples = {
+                    @ExampleObject(summary = "Invalid Address Id",
+                            value = "{\"message\": \"address not found\", \"timestamp\": \"2023-08-26T00:23:12.454577100Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "409", description = "CONFLICT - address already exists to user", content = @Content(examples = {
+                    @ExampleObject(summary = "Conflict user id",
+                            value = "{\"message\": \"at least one attribute needs to be valid\", \"timestamp\": \"2023-08-26T00:58:41.439908900Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR - Something goes wrong", content = @Content(examples = {
+                    @ExampleObject(summary = "Internal Server Error",
+                            value = "{\"message\": \"something goes wrong\", \"timestamp\": \"2023-08-26T00:21:30.426833300Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Address> updateAddressInfoById(@PathVariable UUID id, @RequestBody @Valid AddressUpdateDTO addressUpdateDto) {
         Address address = addressService.updateAddressInfoById(id, addressUpdateDto);
         return ResponseEntity.status(HttpStatus.OK).body(address);
     }
 
+    @Operation(
+            summary = "Delete address (with or without parent relation)",
+            description = "Method for delete a address and returning a JSON response with no content"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(examples = {
+                    @ExampleObject(summary = "Delete an Address.",
+                            value = "")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+
+            @ApiResponse(responseCode = "404", description = "NOT FOUND - Owner id not found", content = @Content(examples = {
+                    @ExampleObject(summary = "Invalid Address Id",
+                            value = "{\"message\": \"address not found\", \"timestamp\": \"2023-08-26T00:23:12.454577100Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "409", description = "CONFLICT - address already exists to user", content = @Content(examples = {
+                    @ExampleObject(summary = "Conflict user id",
+                            value = "{\"message\": \"at least one attribute needs to be valid\", \"timestamp\": \"2023-08-26T00:58:41.439908900Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR - Something goes wrong", content = @Content(examples = {
+                    @ExampleObject(summary = "Internal Server Error",
+                            value = "{\"message\": \"something goes wrong\", \"timestamp\": \"2023-08-26T00:21:30.426833300Z\"}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteAddressById(@NonNull @PathVariable UUID id) {
         JSONObject response = new JSONObject();
